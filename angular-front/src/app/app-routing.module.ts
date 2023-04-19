54ad184ccecca42c84e9c21bf0bd9b9c78a996e1
Zach <zach@weinzierl.us>
2023-04-19 08:31:05 -0500

    Adding the routing and also adding movies service

--- angular-front/src/app/app-routing.module.ts
+++ angular-front/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: MainPageComponent },
+  { path: 'detail/:id', component: MovieDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
