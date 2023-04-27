745b7b0885644d51e0093082e2fd49954ef4b54f
carson.keeping@ndsu.edu <carson.keeping@ndsu.edu>
2023-04-27 11:30:43 -0500

    Adding solid Stars to feedback

--- angular-front/src/app/feedback-section/feedback-section.component.ts
+++ angular-front/src/app/feedback-section/feedback-section.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-feedback-section',
   templateUrl: './feedback-section.component.html',
   styleUrls: ['./feedback-section.component.css']
 })
-export class FeedbackSectionComponent {
+export class FeedbackSectionComponent{
+    Stars: number[] = [1,2,3,4,5];
+    selectedValue = 0;
 
+    countStar(star: number){
+      this.selectedValue = star;
+    }
 }
