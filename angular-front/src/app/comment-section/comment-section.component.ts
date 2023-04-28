274b23d7d2e86ed524c7cadd99ddce81e5efed87
carson.keeping@ndsu.edu <carson.keeping@ndsu.edu>
2023-04-28 15:32:51 -0500

    Adding Input Output for the star rating between feedback and comment

--- angular-front/src/app/comment-section/comment-section.component.ts
+++ angular-front/src/app/comment-section/comment-section.component.ts
@@ -1,54 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-
- 
+import { Component, OnInit, Input,Output, EventEmitter } from '@angular/core';
 
 @Component({
-
   selector: 'app-comment-section',
-
   templateUrl: './comment-section.component.html',
-
   styleUrls: ['./comment-section.component.css']
-
 })
 
 export class CommentSectionComponent implements OnInit {
 
+  @Input() STARS: any = 0;
+  @Output() starsUpdate = new EventEmitter<number>();
+
   comments = [
-
     { user: 'John', message: 'Great article!' },
-
     { user: 'Sarah', message: 'Thanks for sharing.' }
-
   ];
 
- 
-
   newComment = {
-
     user: '',
-
-    message: ''
-
+    message: '',
   };
 
- 
-
   addComment() {
-
     this.comments.push(this.newComment);
-
     this.newComment = {
-
       user: '',
-
       message: ''
-
     };
 
+    this.starsUpdate.emit(0);
   }
-
- 
 
   ngOnInit() {
 
